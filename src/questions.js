f9f125412b6ffbb9be7d18918890dce67e0a1df3
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2017-08-28 15:32:03 -0400

    Initial commit

---
+++ src/questions.js
@@ -0,0 +1,105 @@
+'use strict';
+
+module.exports = {
+    /**
+     * When editing your questions pay attention to your punctuation. Make sure you use question marks or periods.
+     * Make sure the first answer is the correct one. Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+     */
+    "QUESTIONS_EN_US" : [
+        {
+            "What is New Jersey's state animal?": [
+                "Horse",
+                "Fox",
+                "Beaver",
+                "Bear",
+                "Racoon",
+                "Groundhog"
+            ]
+        },
+        {
+            "New Jersey is a leading U.S. producer of which agricultural product?": [
+                "Eggplant",
+                "Strawberry",
+                "Zucchini",
+                "Cranberries",
+                "Spinach",
+                "Tomatoes",
+                "Blueberries"
+            ]
+        },
+        {
+            "What is the highest point in New Jersey?": [
+                "High Point",
+                "Bearfort Mountain",
+                "Smith Benchmark",
+                "Baldpate Mountain"
+            ]
+        },
+        {
+            "Which New Jersey town, has the most shopping malls in the U.S.?": [
+                "Paramus",
+                "Jersey City",
+                "East Rutherford",
+                "Freehold"
+            ]
+        },
+        {
+            "Which of these states border New Jersey, including river borders?": [
+                "Pennsylvania",
+                "Virginia",
+                "Maryland",
+                "Maine"
+            ]
+        },
+        {
+            "New Jersey has how many state parks?": [
+                "40",
+                "29",
+                "42",
+                "36"
+            ]
+        },
+        {
+            "What is New Jersey's state university?": [
+                "Rutgers University",
+                "Princeton University",
+                "Kean University",
+                "Seton Hall University"
+            ]
+        },
+        {
+            "Which New Jersey river was the site of the first submarine ride?": [
+                "Passaic River",
+                "Hudson River",
+                "Hackensack River",
+                "Raritan River"
+            ]
+        },
+        {
+            "How many state forests are located in New Jersey?": [
+                "11",
+                "10",
+                "12",
+                "9"
+            ]
+        },
+        {
+            "Which U.S. President was born in New Jersey?": [
+                "Grover Cleveland",
+                "Andrew Jackson",
+                "John F. Kennedy",
+                "Herbert Hoover"
+            ]
+        },
+        {
+            "What is the capital of New Jersey?": [
+                "Trenton",
+                "Elizabeth",
+                "Newark",
+                "Jersey City",
+                "Paterson",
+                "Edison"
+            ]
+        }
+    ]
+};
